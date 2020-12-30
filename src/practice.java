8c9a15c3c261adfaaab5be82ec7bbcb6da014196
ChungHyeon Kim <asdddq11@gmail.com>
2020-12-30 18:24:34 +0900

    StringBuffer 실습

--- src/practice.java
+++ src/practice.java
@@ -4,17 +4,26 @@ public class practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int iVal = 100;
-		String strVal = String.valueOf(iVal); //int -> String 변환
 		
-		double dVal = 200.0;
-		String strVal2 = dVal + "";
+		StringBuffer sb = new StringBuffer("01");  //01로 시작하는 18칸짜리 StringBuffer생성
+		StringBuffer sb2 = sb.append(23);  //0123
+		sb.append('4').append(56);
+		StringBuffer sb3 = sb.append(78);
+		sb3.append(9.0);
 		
-		double sum = Integer.parseInt("+"+strVal) + Double.parseDouble(strVal2);
-		double sum2 = Integer.valueOf(strVal) + Double.valueOf(strVal2);
+		System.out.println("sb =" +sb);
+		System.out.println("sb2 =" +sb2);
+		System.out.println("sb2 =" +sb3);   //0123456789.0
 		
-		System.out.println(String.join("", strVal,"+", strVal2, "=") +sum);
-		System.out.println(strVal+"+"+strVal2+"="+sum2);
+		System.out.println("sb =" + sb.deleteCharAt(10));
+		System.out.println("sb =" + sb.delete(3, 6));
+		System.out.println("sb =" + sb.insert(3, "abc"));
+		System.out.println("sb =" + sb.replace(6, sb.length(), "END"));
+		
+		System.out.println("capacity=" + sb.capacity());
+		System.out.println("length=" + sb.length());
+		
+		
 	}
 
 }
