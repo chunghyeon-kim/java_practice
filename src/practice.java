57b0f3ac57e6fdf63e410e4d77e288cc9927e959
ChungHyeon Kim <asdddq11@gmail.com>
2021-01-03 14:17:32 +0900

    Comparator practice

--- src/practice.java
+++ src/practice.java
@@ -7,36 +7,20 @@ public class practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Stack st = new Stack();
-		String expression = args[0];
-		
-		System.out.println("expression:" + expression);
-		
-		try{
-			for(int i=0; i<expression.length(); i++) {
-				char ch = expression.charAt(i);
-				
-				if(ch == '(') {
-					st.push(ch + "");
-				} else if (ch==')') {
-					st.pop();
-				}
-			} //end of i loop
-			
-			if(st.isEmpty()) {
-				System.out.println("괄호가 일치합니다.");
-			} else {
-				System.out.println("괄호가 일치하지 않습니다.");
-			}
-		} catch(EmptyStackException e) {
-			System.out.println("괄호가 일치하지 않습니다.");
-		}
+		String[] strArr = {"cat", "Dog", "lion", "tiger"};
 
-		
-		
-		
-		
+		Arrays.sort(strArr, new Descending());
+		System.out.println(Arrays.toString(strArr));
 	}
-
 }
 
+class Descending implements Comparator {
+	public int compare(Object o1, Object o2){
+		if(o1 instanceof Comparable && o2 instanceof Comparable){
+			Comparable c1 = (Comparable)o1;
+			Comparable c2 = (Comparable)o2;
+			return c1.compareTo(c2) *-1;
+		}
+		return -1;
+	}
+}
