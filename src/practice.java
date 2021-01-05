8e6522465c86ee7abf1626a62e83f57b552ad46f
ChungHyeon Kim <asdddq11@gmail.com>
2021-01-05 09:57:46 +0900

    encoding Test

--- src/practice.java
+++ src/practice.java
@@ -7,20 +7,17 @@ public class practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String[] strArr = {"cat", "Dog", "lion", "tiger"};
+		int[][] arr = {
+				{1,2,3,4,5},
+				{1,2,1,1,1},
+				{1,1,3,4,5}
+		};
+        
+		int[][] arr2 = arr.clone();
+		
+		System.out.println(Arrays.deepToString(arr2));
+		
+    }   //end of main
 
-		Arrays.sort(strArr, new Descending());
-		System.out.println(Arrays.toString(strArr));
-	}
-}
+}  //end of class
 
-class Descending implements Comparator {
-	public int compare(Object o1, Object o2){
-		if(o1 instanceof Comparable && o2 instanceof Comparable){
-			Comparable c1 = (Comparable)o1;
-			Comparable c2 = (Comparable)o2;
-			return c1.compareTo(c2) *-1;
-		}
-		return -1;
-	}
-}
