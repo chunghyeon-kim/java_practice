bfc5af4ecd0330f49623230839c713186bc41689
chunghyeon-kim <asdddq11@gmail.com>
2021-01-06 01:15:07 +0900

    coffee shop example practice

--- src/practice.java
+++ src/practice.java
@@ -1,38 +1,67 @@
 import java.util.*;
 
 public class practice {
-
 	public static void main(String[] args) {
-		Card c1 = new Card();
-		Card c2 = new Card();
 
-		System.out.println(c1.equals(c2));
-		System.out.println(c1.hashCode()==c2.hashCode());
 
 
 	}  //end of main
 } //end of class
 
-class Card {
-	String kind;
-	int number;
+class Customer {
+	public void order(String menuName, Menu menu, Barista barista) {
+		Menuitem menuItem = menu.choose(menuName);
+		Coffee coffee = barista.makeCoffee(menuItem);
 
-	Card() {
-		this("SPADE", 1);
 	}
-	Card(String kind, int number){
-		this.kind = kind;
-		this.number = number;
+}
+
+public class MenuItem{
+	private String name;
+	private int price;
+
+	public MenuItem(String name int price) {
+		this.name = name;
+		this.price = price;
 	}
-	public String toString(){
-		return "kind : " + kind + ", number : " + number;
+	public int cost() {
+		return price;
 	}
-	public boolean equals(Object obj){   //equals 오버라이딩 하면 hashCode()도 오버라이딩 해야한다. 규칙!!
-		if(!(obj instanceof Card)) return false;
-		Card c = (Card)obj;
-		return this.kind.equals(c.kind) && this.number == c.number;
+	public String getName() {
+		return name;
 	}
-	public int hashCode(){   //equals가 같으면 hashCode도 같아야 한다. 규칙!!
-		return Objects.hash(kind, number);
+}
+
+class Menu{
+	private List<MenuItem> items;
+
+	public Menu(List<MenuItem> items){
+		this.items = items;
+	}
+
+	public MenuItem choose(String name) {
+		for(MenuItem each : items) {
+			if(each.getName().equals(name)) {
+				return each;
+			}
+		}
+		return null;
+	}
+}  //end of class Menu
+
+class Barista {
+	public Coffee makeCoffee(MenuItem menuItem) {
+		Coffee coffee = new Coffee(menuItem);
+		return coffee;
+	}
+}
+
+class Coffee {
+	private String name;
+	private int price;
+
+	public Coffee(MenuItem menuItem) {
+		this.name = menuItem.getName();
+		this.price = menuItem.cost();
 	}
 }
