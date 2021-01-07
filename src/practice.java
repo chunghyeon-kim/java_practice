cdfa4a1856516031737f49819fea2ee2725fd0e5
chunghyeon-kim <asdddq11@gmail.com>
2021-01-07 22:05:56 +0900

    excercise 11-1

--- src/practice.java
+++ src/practice.java
@@ -1,14 +1,28 @@
+import java.io.File;
 import java.util.*;
 
 public class practice {
 	public static void main(String[] args) {
-		System.out.println(1);
-		System.out.println(2);
-		System.out.println(3);
-//		System.out.println(0/0);
-		System.out.println(4);
 
+		ArrayList list = new ArrayList();
+		list.add(3);
+		list.add(6);
+		list.add(2);
+		list.add(2);
+		list.add(2);
+		list.add(7);
+
+		HashSet set = new HashSet(list);
+		TreeSet tset = new TreeSet(set);
+		Stack stack = new Stack();
+		stack.addAll(tset);
+
+		while(!stack.empty())
+			System.out.println(stack.pop());
 	}
 
-}
+
+}  //end of class
+
+
 
