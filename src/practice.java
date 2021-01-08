debf5826505048e6c92775667c811cbcc48526d9
chunghyeon-kim <asdddq11@gmail.com>
2021-01-08 17:26:49 +0900

    card practice

--- src/practice.java
+++ src/practice.java
@@ -4,23 +4,43 @@ import java.util.*;
 public class practice {
 	public static void main(String[] args) {
 
-		ArrayList list = new ArrayList();
-		list.add(3);
-		list.add(6);
-		list.add(2);
-		list.add(2);
-		list.add(2);
-		list.add(7);
+		
+	public static String straightCheck(Card[] chArr){
+		//1."STRAIGHT" -  카드의 숫자가 연속인 경우. 1,2,3,4,5 또는 5,6,7,8,9
+		int[] kindCount = new int[4];
+		int[] numberCount = new int[13];
 
-		HashSet set = new HashSet(list);
-		TreeSet tset = new TreeSet(set);
-		Stack stack = new Stack();
-		stack.addAll(tset);
+		for(int i=0; i<chArr.length; i++){
+			kindCount[chArr[i].kind-1]++;
+			numberCount[chArr[i].number-1]++;
+		}
 
-		while(!stack.empty())
-			System.out.println(stack.pop());
-	}
+		//숫자 조건 어떻게?
 
+		boolean result1 = true;
+		boolean result2 = true;
+
+		for(int i=0; i<5; i++){ if (numberCount[i]!=1) result1 = result1 && false; }
+		for(int i=3; i<7; i++){ if (numberCount[i]!=1) result2 = result2 && false; }
+//        if((numberCount[0]==1 &&
+//                numberCount[1]==1 &&
+//                numberCount[2]==1 &&
+//                numberCount[3]==1 &&
+//                numberCount[4]==1 ||
+//
+//                numberCount[4]==1 &&
+//                        numberCount[5]==1 &&
+//                        numberCount[6]==1 &&
+//                        numberCount[7]==1 &&
+//                        numberCount[8]==1))
+		if(result1 || result2)
+			return "STRAIGHT";
+		else return "NOT STRAIGHT";
+
+
+		//10. "FULL HOUSE" -
+
+	} //end of method straightCheck
 
 }  //end of class
 
