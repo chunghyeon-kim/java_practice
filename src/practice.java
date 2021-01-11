3be96d58887f8961830035dc8baf8457270590e3
chunghyeon-kim <asdddq11@gmail.com>
2021-01-11 17:02:33 +0900

    MyVector practice

--- src/practice.java
+++ src/practice.java
@@ -1,53 +1,10 @@
 import java.io.File;
 import java.util.*;
 
-class Product {
-	int price;
-	int bonusPoint;
-
-	Product(int price){
-		this.price = price;
-		this.bonusPoint = (int)(price/10.0);
-	}
-}
-
-class Tv extends Product{
-	Tv(){
-		super(100);
-	}
-	public String toString(){return "Tv";}
-}
-
-class Computer extends Product{
-	Computer(){
-		super(200);
-	}
-	public String toString(){return "Computer";}
-}
-
-class Buyer{
-	int money = 1000;
-	int bonusPoint = 0;
-
-	void buy(Product p){
-		if(p.price>money){
-			System.out.println("잔액이 부족하여 물건을 살 수 없습니다");
-			return;
-		}
-		money -=p.price;
-		bonusPoint +=p.bonusPoint;
-		System.out.println(p + "을/를 구입하셨습니다");
-	}
-}
 
 public class practice {
 	public static void main(String[] args) {
-		Buyer b = new Buyer();
-		b.buy(new Tv());
-		b.buy(new Computer());
-
-		System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
-		System.out.println("현재 보너스점수는 " + b.bonusPoint + "점입니다.");
+		
 
 	} //end of main
 
@@ -57,4 +14,94 @@ public class practice {
 }  //end of class
 
 
+class MyVector{
+	//배열기반
+	int size;
+	Object[] objArr;
 
+	MyVector(int capacity){   //생성자
+		objArr = new Object[capacity];
+	}
+
+	MyVector(){
+		this(16);
+	}
+
+	int size(){
+		int tmp = 0;
+		for(int i=0; i<objArr.length; i++){
+			if(objArr[i]!=null) tmp++;
+		}
+		return tmp;
+	}
+
+	int capacity(){
+		return objArr.length;
+	}
+
+	boolean isEmpty(){
+		for(int i=0; i<objArr.length; i++){
+			if(objArr[i]!=null) return false;
+		}
+		return true;
+	}
+
+	void clear(){
+		for(int i=0; i<objArr.length; i++){
+			objArr[i]=null;
+		}
+	}
+
+	Object get(int index){
+		return objArr[index];
+	}
+
+	int indexOf(Object obj){
+		//equals() 오버라이딩???..... 모르겠다
+		//일단 Object배열에 문자열이 들어있는 상황에서 메서드 작성
+		for(int i=0; i<this.size(); i++){
+			if(objArr[i].equals(obj))
+				return i;
+		}
+		return -1;
+	}
+
+	void add(Object obj){
+		for(int i=0; i<objArr.length; i++){
+			if(objArr[i]==null) {objArr[i]=obj; return;}
+		}
+		System.out.println("FULL"); return;
+	}
+
+	void setCapacity(int capacity){
+		Object[] tmp = new Object[capacity];
+		for(int i=0; i<this.objArr.length; i++){
+			tmp[i] = this.objArr[i];
+		}
+		this.objArr = tmp;
+	}
+
+	void ensureCapacity(int minCapacity){  //size(),setCapacity() 이용할 수 있을 것 같다.
+		//용량 확보 - 현재 들어있는 객체 개수 + minCapacity 크기의 배열 생성, 복사, 참조변경
+		//                size()
+		//          [------------setCapacity-------]
+
+		Object[] tmp = new Object[this.size() + minCapacity];  //용량확보한 배열 생성
+		for(int i=0; i<this.size(); i++){                      //원래있던 자료 복사
+			tmp[i] = objArr[i];
+		}
+		objArr = tmp;                                          //참조변
+
+	}
+
+	Object remove(int index){    //index+1 번째부터 끝까지 앞으로 한칸씩 당기고 마지막은 null대입
+		for(int i=index; i<this.size(); i++){
+			objArr[i] = objArr[i+1];   //차례대로 덮어씌우기
+		}
+		objArr[this.size()] = null;          //마지막은 null 대입
+		return 1;
+	}
+
+
+
+}  //end of class MyVeector
