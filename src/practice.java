3372cd426a762166bb8a901f95c8d1ba25dcabe1
chunghyeon-kim <asdddq11@gmail.com>
2021-01-17 20:33:53 +0900

    InputStream OutputStream practice

--- src/practice.java
+++ src/practice.java
@@ -9,24 +9,22 @@ public class practice {
 public static void main(String[] args)  {
 		byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};
 		byte[] outSrc = null;
-		byte[] temp = new byte[10];
 
-	ByteArrayInputStream input = null;
-	ByteArrayOutputStream output = null;
+		ByteArrayInputStream input = null;
+		ByteArrayOutputStream output = null;
 
-	input = new ByteArrayInputStream(inSrc);
-	output = new ByteArrayOutputStream();
+		input = new ByteArrayInputStream(inSrc);
+		output = new ByteArrayOutputStream();
 
-	input.read(temp, 0, temp.length);
-	output.write(temp, 5, 5);
+		int data = 0;
 
-	outSrc = output.toByteArray();
+		while((data = input.read())!=-1)
+			output.write(data);
 
-	System.out.println("Input source  :" + Arrays.toString(inSrc));
-	System.out.println("temp          :" + Arrays.toString(temp));
+		outSrc = output.toByteArray();
 
-
-
+	System.out.println("Input Source : " + Arrays.toString(inSrc));
+	System.out.println("Output Source : " + Arrays.toString(outSrc));
 
 	} //end of main
 
