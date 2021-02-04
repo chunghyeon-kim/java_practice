f385e036ef193520addf66670e28bc8b4d97057d
chunghyeon-kim <asdddq11@gmail.com>
2021-02-04 23:02:56 +0900

    String practice

--- src/practice.java
+++ src/practice.java
@@ -8,13 +8,21 @@ import java.util.stream.*;
 
 public class practice {
 public static void main(String[] args)  {
+	char ch = '@';
+	boolean check = true;
+	if(  !(('a'<=ch && ch<='z') || ('A'<=ch && ch<='Z') || ('0'<=ch && ch<='9'))    )
 
-	String str = " a b c ";
-	String str2 = str.replace(" ", "");
+	{check=false;}
+	System.out.println(check);
 
-	System.out.println(
-		str2
-	);
+	String str = "abc@#$";
+	boolean check2 = true;
+	for(int i=0; i<str.length(); i++) {
+		char ch2 = str.charAt(i);
+		if(  !(('a'<=ch2 && ch2<='z') || ('A'<=ch2 && ch2<='Z') || ('0'<=ch2 && ch2<='9'))    )
+		{check2=false;}
+	} System.out.println("check2=" + check2);
+
 
 	} //end of main
 
