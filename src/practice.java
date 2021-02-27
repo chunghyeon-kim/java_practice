0b0055dbe1ff6de885444056d8b583bcf6ffb422
chunghyeon-kim <asdddq11@gmail.com>
2021-02-28 08:21:17 +0900

    enum practice

--- src/practice.java
+++ src/practice.java
@@ -6,20 +6,49 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.*;
 
+enum Direction { EAST, SOUTH, WEST, NORTH }
+
 public class practice {
 public static void main(String[] args)  {
-    System.out.println(pageCalculation(1));
-    System.out.println(pageCalculation(8));
+    Direction d1 = Direction.EAST;
+    Direction d2 = Direction.valueOf("WEST");
+    Direction d3 = Enum.valueOf(Direction.class, "EAST");
+
+    System.out.println("d1="+d1);
+    System.out.println("d2="+d2);
+    System.out.println("d3="+d3);
+
+    System.out.println("d1==d2 ? "+ (d1==d2));
+    System.out.println("d1==d3 ? "+ (d1==d3));
+    System.out.println("d1.equals(d3) ? " + d1.equals(d3));
+    System.out.println("d1.compareTo(d3) ?"+ (d1.compareTo(d3)));
+    System.out.println("d1.compareTo(d2) ?"+ (d1.compareTo(d2)));
+
+    switch(d1) {
+        case EAST:
+            System.out.println("The direction is EAST."); break;
+        case SOUTH:
+            System.out.println("The direction is SOUTH."); break;
+        case NORTH:
+            System.out.println("The direction is NORTH."); break;
+        case WEST:
+            System.out.println("The direction is WEST."); break;
+        default:
+            System.out.println("Invalid direction."); break;
+    }
+
+    Direction[] dArr = Direction.values();
+
+    for(Direction d : dArr)
+        System.out.printf("%s=%d%n", d.name(), d.ordinal());
+
+
 
 
 
 
 
 } //end of main
-
-    static int pageCalculation (int num) {
-        return (int)(Math.ceil(num / 10.0)) * 10;
-    }
 
 
 }  //end of class
