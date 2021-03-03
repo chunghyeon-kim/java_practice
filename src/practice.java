6d35f3fa9fda97aef839b436a8f78b0c13fc8197
chunghyeon-kim <asdddq11@gmail.com>
2021-03-03 23:50:11 +0900

    practice

--- src/practice.java
+++ src/practice.java
@@ -16,11 +16,6 @@ public static void main(String[] args)  {
 
         System.out.print("소요시간1:" + (System.currentTimeMillis() - startTime));
 
-        for(int i=0; i<300; i++)
-            System.out.printf("%s", new String("|"));
-
-        System.out.println("소요시간2:" + (System.currentTimeMillis() - startTime));
-
 
 
 
