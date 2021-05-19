24d8f798cb2689736bfe2f3950b2f468c6f9977f
chunghyeon-kim <asdddq11@gmail.com>
2021-05-19 10:00:57 +0900

    calendar practice

--- src/practice.java
+++ src/practice.java
@@ -1,5 +1,6 @@
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -8,21 +9,32 @@ import java.util.stream.Stream;
 
 public class practice {
     public static void main(String[] args) {
-        IntStream intStream = IntStream.rangeClosed(1, 10); // 1~10
-        intStream.filter(i -> i%2 != 0 && i%3 != 0).forEach(System.out::print);
+        Calendar date = Calendar.getInstance();
+        date.set(2019, 7, 31);  //2019년 8월 31일
 
-        System.out.println();
+        System.out.println(toString(date));
+        System.out.println("= 1일 후 =");
+        date.add(Calendar.DATE, 1);
+        System.out.println(toString(date));
 
-        intStream = IntStream.rangeClosed(1, 10); // 1~10
-        intStream.filter(i -> i%2 != 0).filter(i -> i%3 != 0).forEach(System.out::print);
+        System.out.println("= 6달 전 =");
+        date.add(Calendar.MONTH, -6);
+        System.out.println(toString(date));
 
-        System.out.println();
+        // add()와 달리 roll()은 다른 필드에 영향을 주지 않는다.
+        System.out.println("= 31일 후(roll) =");
+        date.roll(Calendar.DATE, 31);
+        System.out.println(toString(date));
 
-        intStream = IntStream.of(1,2,2,3,3,3,4,5,5,6,7,7,8,8,8);
-        intStream.distinct().forEach(System.out::print);
-
+        System.out.println("= 31일 후(add) =");
+        date.add(Calendar.DATE, 31);
+        System.out.println(toString(date));
 
     } //end of main
+
+    public static String toString(Calendar date) {
+        return date.get(Calendar.YEAR) + "년 " + (date.get(Calendar.MONTH) + "월 " + date.get(Calendar.DATE) + "일");
+    }
 }  //end of class
 
 
